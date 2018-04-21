bb445d9a519589926f75f26715f43f8c2e6311e6
JuanDiegost <molinajuan149@gmail.com>
2018-04-20 19:49:12 -0500

    Parece que funca

---
+++ Funcionando/Client/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <dirent.h>
+#include "Client.h"
+//#include <filesystem>
+//namespace fs = std::filesystem;
+using namespace std;
+
+
+
+int main()
+{
+    int port = 5000;
+    cout<<"Cliente"<<endl;
+    cout<<"Puerto: "<<endl;
+    cin>>port;
+
+    cout<<"Nombre del host o ip: ";
+    string host;
+    cin>>host;
+    fflush(stdin);
+
+    Client *client = new Client(port, host);
+    client->connectServer();
+
+
+
+}
